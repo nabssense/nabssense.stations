2b6cc8ffd2f4fc7cefc7b87f50b0fcaa0aff1b7e
Naufal Syihabuddin <nabil723>
2024-03-24 22:52:03 +0700

    testing kedua

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Zen-Kaku-Gothic-New': "Zen Kaku Gothic New",
+      },
+    },
+  },
+  plugins: [],
+}
+
